1136b0a7998ada22d476e19df0ffafc726d0bafa
Paweł Renc <rencpawe@gmail.com>
2019-10-31 17:25:43 +0100

    C imp/implementation (#1)
    Add c implementation

---
+++ sequential_c_imp/main.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+
+#define ROWS 100
+#define COLS 100
+#define CELL_SIZE_X 10
+#define CELL_SIZE_Y 10
+#define THICKNESS 50
+
+#define Syinitial 0.1
+#define Kinitial  0.0000125
+
+#define headFixed 50
+#define headCalculated 50
+
+#define SIMULATION_ITERATIONS 100
+
+float delta_t_ = 4000;
+float qw = 0.001;
+
+int posSy = ROWS / 2;
+int posSx = COLS / 2;
+
+struct {
+    float head[ROWS][COLS];
+    float Sy[ROWS][COLS];
+    float K[ROWS][COLS];
+    float Source[ROWS][COLS];
+} ca;
+
+void init_ca();
+
+void simulation_step();
+
+void transition_function(int i, int j);
+
+void count_q(float *pDouble, int i, int j, int i1, int j1);
+
+void write_heads_to_file();
+
+int main() {
+    init_ca();
+
+    for (int i = 0; i < SIMULATION_ITERATIONS; i++) {
+        simulation_step();
+    }
+
+    write_heads_to_file();
+
+    return 0;
+}
+
+void write_heads_to_file() {
+    FILE *fp;
+    fp = fopen("heads_ca.txt", "w");
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            fprintf(fp, "%f, ", ca.head[i][j]);
+        }
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
+}
+
+void simulation_step() {
+    for (int i = 0; i < ROWS; i++)
+        if (i != 0 && i != ROWS - 1)
+            for (int j = 0; j < COLS; j++) {
+                transition_function(i, j);
+            }
+}
+
+void transition_function(int i, int j) {
+
+    float Q = 0;
+
+    if (i - 1 >= 0) {
+        count_q(&Q, i, j, i - 1, j);
+    }
+    if (j - 1 >= 0) {
+        count_q(&Q, i, j, i, j - 1);
+    }
+    if (i + 1 < ROWS) {
+        count_q(&Q, i, j, i + 1, j);
+    }
+    if (j + 1 < COLS) {
+        count_q(&Q, i, j, i, j + 1);
+    }
+
+    Q -= ca.Source[i][j];
+    float area = CELL_SIZE_X * CELL_SIZE_Y;
+    float ht1 = (Q * delta_t_) / (area * ca.Sy[i][j]);
+
+    ca.head[i][j] += ht1;
+
+}
+
+void count_q(float *pDouble, int i, int j, int i1, int j1) {
+    float diff_head = ca.head[i1][j1] - ca.head[i][j];
+    float tmp_t = ca.K[i1][j1] * THICKNESS;
+
+    *pDouble += diff_head * tmp_t;
+}
+
+
+void init_ca() {
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++) {
+            ca.head[i][j] = headFixed;
+            if (j == COLS - 1) {
+                ca.head[i][j] = headCalculated;
+            }
+            ca.Sy[i][j] = Syinitial;
+            ca.K[i][j] = Kinitial;
+            ca.Source[i][j] = 0;
+        }
+
+    ca.Source[posSy][posSx] = qw;
+}
