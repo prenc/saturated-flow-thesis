fbdd0db63569282425ec5a6a34373dc179e19f7c
Paweł Renc <rencpawe@gmail.com>
2019-10-31 17:31:58 +0100

    Change floats to doubles in c implementation

--- sequential_c_imp/main.c
+++ sequential_c_imp/main.c
@@ -12,19 +12,19 @@
 #define headFixed 50
 #define headCalculated 50
 
-#define SIMULATION_ITERATIONS 100
+#define SIMULATION_ITERATIONS 1000
 
-float delta_t_ = 4000;
-float qw = 0.001;
+double delta_t_ = 4000;
+double qw = 0.001;
 
 int posSy = ROWS / 2;
 int posSx = COLS / 2;
 
 struct {
-    float head[ROWS][COLS];
-    float Sy[ROWS][COLS];
-    float K[ROWS][COLS];
-    float Source[ROWS][COLS];
+    double head[ROWS][COLS];
+    double Sy[ROWS][COLS];
+    double K[ROWS][COLS];
+    double Source[ROWS][COLS];
 } ca;
 
 void init_ca();
@@ -33,7 +33,7 @@ void simulation_step();
 
 void transition_function(int i, int j);
 
-void count_q(float *pDouble, int i, int j, int i1, int j1);
+void count_q(double *pDouble, int i, int j, int i1, int j1);
 
 void write_heads_to_file();
 
@@ -55,7 +55,7 @@ void write_heads_to_file() {
 
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            fprintf(fp, "%f, ", ca.head[i][j]);
+            fprintf(fp, "%lf, ", ca.head[i][j]);
         }
         fprintf(fp, "\n");
     }
@@ -73,7 +73,7 @@ void simulation_step() {
 
 void transition_function(int i, int j) {
 
-    float Q = 0;
+    double Q = 0;
 
     if (i - 1 >= 0) {
         count_q(&Q, i, j, i - 1, j);
@@ -89,16 +89,16 @@ void transition_function(int i, int j) {
     }
 
     Q -= ca.Source[i][j];
-    float area = CELL_SIZE_X * CELL_SIZE_Y;
-    float ht1 = (Q * delta_t_) / (area * ca.Sy[i][j]);
+    double area = CELL_SIZE_X * CELL_SIZE_Y;
+    double ht1 = (Q * delta_t_) / (area * ca.Sy[i][j]);
 
     ca.head[i][j] += ht1;
 
 }
 
-void count_q(float *pDouble, int i, int j, int i1, int j1) {
-    float diff_head = ca.head[i1][j1] - ca.head[i][j];
-    float tmp_t = ca.K[i1][j1] * THICKNESS;
+void count_q(double *pDouble, int i, int j, int i1, int j1) {
+    double diff_head = ca.head[i1][j1] - ca.head[i][j];
+    double tmp_t = ca.K[i1][j1] * THICKNESS;
 
     *pDouble += diff_head * tmp_t;
 }
