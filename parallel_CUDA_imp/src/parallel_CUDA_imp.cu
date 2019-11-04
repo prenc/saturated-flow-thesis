3a0de92d5efef9f555663716b83e4476e841694d
Tomasz Pęcak <pecatoma@gmail.com>
2019-11-05 00:52:54 +0100

    Add second dimension to data mapping  (#4)
    * Add second dim to kernel, doesnt work, debug required
    
    * Make CUDA 2D imp working

--- parallel_CUDA_imp/src/parallel_CUDA_imp.cu
+++ parallel_CUDA_imp/src/parallel_CUDA_imp.cu
@@ -1,25 +1,18 @@
-/*
- ============================================================================
- Name        : parallel_CUDA_imp.cu
- Author      : Tomasz Pęcak and Paweł Renc
- Version     :
- Copyright   : Your copyright notice
- Description : CUDA compute reciprocals
- ============================================================================
- */
-
 #include <iostream>
 #include <numeric>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 //MODEL PARAMS
 
 #define ROWS 100
 #define COLS 100
+
 #define CELL_SIZE_X 10
 #define CELL_SIZE_Y 10
 #define AREA CELL_SIZE_X*CELL_SIZE_Y
+
 #define THICKNESS 50
 
 #define Syinitial 0.1
@@ -29,7 +22,7 @@
 #define headCalculated 50
 
 #define SIMULATION_ITERATIONS 1000
-#define BLOCK_SIZE 256
+#define BLOCK_SIZE 16
 
 #define DELTA_T 4000;
 double qw = 0.001;
@@ -53,48 +46,47 @@ static void CheckCudaErrorAux(const char *, unsigned, const char *, cudaError_t)
 
 #define CUDA_CHECK_RETURN(value) CheckCudaErrorAux(__FILE__,__LINE__, #value, value)
 
-/**
- * CUDA kernel that computes simulation step
- */
 __global__ void simulation_step_kernel(struct CA *d_ca, double *d_write_head) {
-    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < ROWS * COLS)
-        if (idx / COLS != 0 && idx / COLS != ROWS - 1) {
+    unsigned idx_x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned idx_y = blockIdx.y * blockDim.y + threadIdx.y;
+    unsigned idx_g = idx_y * COLS + idx_x;
+
+    if (idx_x < COLS && idx_y < ROWS)
+        if (idx_y != 0 && idx_y != ROWS - 1) {
             double Q = 0;
+            double diff_head;
+            double tmp_t;
 
-            if (idx % COLS >= 1) {
-                double diff_head = d_ca->head[idx - 1] - d_ca->head[idx];
-                double tmp_t = d_ca->K[idx] * THICKNESS;
+            if (idx_x >= 1) {
+                diff_head = d_ca->head[idx_g - 1] - d_ca->head[idx_g];
+                tmp_t = d_ca->K[idx_g] * THICKNESS;
                 Q += diff_head * tmp_t;
             }
-            if (idx >= COLS) {
-                double diff_head = d_ca->head[idx - COLS] - d_ca->head[idx];
-                double tmp_t = d_ca->K[idx] * THICKNESS;
+            if (idx_y >= 1) {
+                diff_head = d_ca->head[(idx_y - 1) * COLS + idx_x] - d_ca->head[idx_g];
+                tmp_t = d_ca->K[idx_g] * THICKNESS;
                 Q += diff_head * tmp_t;
             }
-            if (idx % COLS + 1 < COLS) {
-                double diff_head = d_ca->head[idx + 1] - d_ca->head[idx];
-                double tmp_t = d_ca->K[idx] * THICKNESS;
+            if (idx_x + 1 < COLS) {
+                diff_head = d_ca->head[idx_g + 1] - d_ca->head[idx_g];
+                tmp_t = d_ca->K[idx_g] * THICKNESS;
                 Q += diff_head * tmp_t;
             }
-            if (idx + COLS < COLS * ROWS) {
-                double diff_head = d_ca->head[idx + COLS] - d_ca->head[idx];
-                double tmp_t = d_ca->K[idx] * THICKNESS;
+            if (idx_y + 1 < ROWS) {
+                diff_head = d_ca->head[(idx_y + 1) * COLS + idx_x] - d_ca->head[idx_g];
+                tmp_t = d_ca->K[idx_g] * THICKNESS;
                 Q += diff_head * tmp_t;
             }
 
-            Q -= d_ca->Source[idx];
+            Q -= d_ca->Source[idx_g];
 
             double ht1 = Q * DELTA_T;
-            double ht2 = AREA * d_ca->Sy[idx];
+            double ht2 = AREA * d_ca->Sy[idx_g];
 
-            d_write_head[idx] = d_ca->head[idx] + ht1 / ht2;
+          d_write_head[idx_g] = d_ca->head[idx_g] + ht1 / ht2;
         }
 }
 
-/**
- * Host function that copies the data
- */
 void copy_data_from_CPU_to_GPU() {
     double *d_read_head, *d_read_Sy, *d_read_K, *d_read_Source;
 
@@ -115,7 +107,8 @@ void copy_data_from_CPU_to_GPU() {
     CUDA_CHECK_RETURN(cudaMemcpy(&(d_read_ca->K), &d_read_K, sizeof(d_read_ca->K), cudaMemcpyHostToDevice));
 
     CUDA_CHECK_RETURN(cudaMemcpy(d_read_Source, h_ca.Source, sizeof(double) * ROWS * COLS, cudaMemcpyHostToDevice));
-    CUDA_CHECK_RETURN(cudaMemcpy(&(d_read_ca->Source), &d_read_Source, sizeof(d_read_ca->Source), cudaMemcpyHostToDevice));
+    CUDA_CHECK_RETURN(
+            cudaMemcpy(&(d_read_ca->Source), &d_read_Source, sizeof(d_read_ca->Source), cudaMemcpyHostToDevice));
 
     CUDA_CHECK_RETURN(cudaMemcpy(d_write_head, h_ca.head, sizeof(double) * ROWS * COLS, cudaMemcpyHostToDevice));
 
@@ -126,14 +119,19 @@ void copy_data_from_GPU_to_CPU() {
 }
 
 void perform_simulation_on_GPU() {
-    const int blockCount = (ROWS * COLS) / BLOCK_SIZE + 1;
+
+    dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
+    const int blockCount = (ROWS * COLS) / (BLOCK_SIZE * BLOCK_SIZE) + 1;
+    double gridSize = sqrt(blockCount) + 1;
+    dim3 blockCount2D(gridSize, gridSize);
     for (int i = 0; i < SIMULATION_ITERATIONS; i++) {
-        simulation_step_kernel << < blockCount, BLOCK_SIZE >> > (d_read_ca, d_write_head);
+        simulation_step_kernel << < blockCount2D, blockSize >> > (d_read_ca, d_write_head);
 
         cudaDeviceSynchronize();
 
         double *tmp1 = d_write_head;
-        CUDA_CHECK_RETURN(cudaMemcpy(&d_write_head, &(d_read_ca->head), sizeof(d_read_ca->head), cudaMemcpyDeviceToHost));
+        CUDA_CHECK_RETURN(
+                cudaMemcpy(&d_write_head, &(d_read_ca->head), sizeof(d_read_ca->head), cudaMemcpyDeviceToHost));
         CUDA_CHECK_RETURN(cudaMemcpy(&(d_read_ca->head), &tmp1, sizeof(tmp1), cudaMemcpyHostToDevice));
     }
 }
