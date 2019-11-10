93cfced6d7883082854f9c10ebeb608f530f8e62
Paweł Renc <rencpawe@gmail.com>
2019-11-10 21:48:05 +0100

    Remove wrong path when compiling

--- parallel_CUDA_imp/src/run_tests.sh
+++ parallel_CUDA_imp/src/run_tests.sh
@@ -8,7 +8,7 @@ files_to_compile=("${PWD}"/memory_*.cu)
 
 for file_name_path in "${files_to_compile[@]}"; do
   file_name=${file_name_path##*/}
-  nvcc "${file_name}" -o /x/"${file_name%\.cu}"_compiled
+  nvcc "${file_name}" -o "${file_name%\.cu}"_compiled
 done
 
 files_to_test=("${PWD}"/memory_*_compiled)
