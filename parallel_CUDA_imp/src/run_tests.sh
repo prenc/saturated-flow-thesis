3c9c0acc25527bf956261d8ed16579b52a0f228b
Paweł Renc <rencpawe@gmail.com>
2019-11-10 19:16:28 +0100

    Add server version

--- parallel_CUDA_imp/src/run_tests.sh
+++ parallel_CUDA_imp/src/run_tests.sh
@@ -4,7 +4,13 @@ files_to_compile=("${PWD}"/memory_*.cu)
 
 for file_name_path in "${files_to_compile[@]}"; do
   file_name=${file_name_path##*/}
-  docker run -v "${PWD}":/x nvidia/cuda:latest nvcc /x/"${file_name}" -o /x/"${file_name%\.cu}"_compiled
+  nvcc "${file_name}" -o /x/"${file_name%\.cu}"_compiled
 done
 
 files_to_test=("${PWD}"/memory_*_compiled)
+
+for file_name_path in "${files_to_test[@]}"; do
+  file_name=${file_name_path##*/}
+  nvprof ./"${file_name}" 2> "${file_name}"_profiling
+done
+
