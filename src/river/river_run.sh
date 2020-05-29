afe041bc60c80330c5ae83c9cbbc823e281d161b
Tomasz Pecak <pecatoma@gmail.com>
2020-05-29 12:27:43 +0200

    Use test script in river run script

--- src/river/river_run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-nvcc river_unified.cu -o river_exe
-./river_exe
-./river_visualization.py
---
+++ visualization_python/river_run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+../tests/model_analyzer.py river
+./river_visualization.py
