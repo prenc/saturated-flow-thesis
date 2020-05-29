8077d54eb27ddaab62fd1310c3704721b58134e3
Tomasz Pecak <pecatoma@gmail.com>
2020-05-29 12:18:11 +0200

    Move river output to separate directory

--- src/parallel/common/write_heads_to_file.c
+++ src/parallel/common/write_heads_to_file.c
@@ -7,26 +7,30 @@
 struct stat st = {0};
 
 void write_to_file(double *array, std::string fileName);
-void create_output_dir();
+void create_output_dir(std::string path);
 
 void write_heads_to_file(double *head, std::string test_name) {
-	create_output_dir();
-	std::string fileName = "./output/"+test_name+"_"+std::to_string(BLOCK_SIZE)+"_"
+	std::string output_path= "./output/";
+	create_output_dir(output_path);
+	std::string fileName = output_path+test_name+"_"+std::to_string(BLOCK_SIZE)+"_"
 	                       +std::to_string(CA_SIZE)+"_"+std::to_string(SIMULATION_ITERATIONS);
 	write_to_file(head, fileName);
 }
 
 void write_river_heads_to_file(double *head, double river_head, int day) {
-	create_output_dir();
-	std::string fileName = "./output/river_"
+	std::string output_path= "./output/";
+	create_output_dir(output_path);
+	output_path= "./output/river/";
+	create_output_dir(output_path);
+	std::string fileName = output_path
 									+ std::to_string(river_head)
 									+ "_" + std::to_string(day);
 	write_to_file(head, fileName);
 }
 
-void create_output_dir(){
-	if (stat("output", &st) == -1) {
-		mkdir("./output", 0700);
+void create_output_dir(std::string path){
+	if (stat(path.c_str(), &st) == -1) {
+		mkdir(path.c_str(), 0700);
 	}
 }
 
