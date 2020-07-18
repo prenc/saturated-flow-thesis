d00d694ca5dde4fc6037a59f49205e6d4c8ad97a
Tomasz <pecatoma@gmail.com>
2020-07-18 14:39:38 +0200

    Fix issues in loops imps caused by fatuous copypasting

--- src/parallel/active_cells/active_cells_loop.cu
+++ src/parallel/active_cells/active_cells_loop.cu
@@ -7,163 +7,163 @@ __device__ int active_cells_idx[ROWS * COLS];
 __managed__ int dev_active_cells_count = 0;
 
 __device__ void my_push_back(int cellIdx) {
-	int insert_ptr = atomicAdd(&dev_active_cells_count, 1);
-	active_cells_idx[insert_ptr] = cellIdx;
+    int insert_ptr = atomicAdd(&dev_active_cells_count, 1);
+    active_cells_idx[insert_ptr] = cellIdx;
 }
 
 __global__ void simulation_step_kernel(struct CA d_ca, double *d_write_head) {
-	int activeBlockCount = ceil((double) dev_active_cells_count / (BLOCK_SIZE * BLOCK_SIZE));
-	int activeGridSize = ceil(sqrtf(activeBlockCount));
+    int activeBlockCount = ceil((double) dev_active_cells_count / (BLOCK_SIZE * BLOCK_SIZE));
+    int activeGridSize = ceil(sqrtf(activeBlockCount));
 
-	unsigned ac_idx_x = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned ac_idx_y = blockIdx.y * blockDim.y + threadIdx.y;
-	unsigned ac_idx_g = ac_idx_y * blockDim.x * activeGridSize + ac_idx_x;
+    unsigned ac_idx_x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned ac_idx_y = blockIdx.y * blockDim.y + threadIdx.y;
+    unsigned ac_idx_g = ac_idx_y * blockDim.x * activeGridSize + ac_idx_x;
 
-	if (ac_idx_g < dev_active_cells_count) {
-		unsigned idx_g = active_cells_idx[ac_idx_g];
-		unsigned idx_x = idx_g % COLS;
-		unsigned idx_y = idx_g / COLS;
-		if (idx_y != 0 && idx_y != ROWS - 1) {
-			double Q, diff_head, tmp_t;
-			for (int i = 0; i < KERNEL_LOOP_SIZE; i++) {
-				if (i == KERNEL_LOOP_SIZE - 1){
-					Q = 0;
-				}
-				if (idx_x >= 1) {
-					diff_head = d_ca.head[idx_g - 1] - d_ca.head[idx_g];
-					tmp_t = d_ca.K[idx_g] * THICKNESS;
-					Q += diff_head * tmp_t;
-				}
-				if (idx_y >= 1) {
-					diff_head = d_ca.head[(idx_y - 1) * COLS + idx_x] - d_ca.head[idx_g];
-					tmp_t = d_ca.K[idx_g] * THICKNESS;
-					Q += diff_head * tmp_t;
-				}
-				if (idx_x + 1 < COLS) {
-					diff_head = d_ca.head[idx_g + 1] - d_ca.head[idx_g];
-					tmp_t = d_ca.K[idx_g] * THICKNESS;
-					Q += diff_head * tmp_t;
-				}
-				if (idx_y + 1 < ROWS) {
-					diff_head = d_ca.head[(idx_y + 1) * COLS + idx_x] - d_ca.head[idx_g];
-					tmp_t = d_ca.K[idx_g] * THICKNESS;
-					Q += diff_head * tmp_t;
-				}
-			}
-
-			Q -= d_ca.Source[idx_g];
-
-			double ht1 = Q * DELTA_T;
-			double ht2 = AREA * d_ca.Sy[idx_g];
-
-			d_write_head[idx_g] = d_ca.head[idx_g] + ht1 / ht2;
+    if (ac_idx_g < dev_active_cells_count) {
+	unsigned idx_g = active_cells_idx[ac_idx_g];
+	unsigned idx_x = idx_g % COLS;
+	unsigned idx_y = idx_g / COLS;
+	if (idx_y != 0 && idx_y != ROWS - 1) {
+	    double Q, diff_head, tmp_t;
+	    for (int i = 0; i < KERNEL_LOOP_SIZE; i++) {
+		if (i == KERNEL_LOOP_SIZE - 1){
+		    Q = 0;
 		}
+		if (idx_x >= 1) {
+		    diff_head = d_ca.head[idx_g - 1] - d_ca.head[idx_g];
+		    tmp_t = d_ca.K[idx_g] * THICKNESS;
+		    Q += diff_head * tmp_t;
+		}
+		if (idx_y >= 1) {
+		    diff_head = d_ca.head[(idx_y - 1) * COLS + idx_x] - d_ca.head[idx_g];
+		    tmp_t = d_ca.K[idx_g] * THICKNESS;
+		    Q += diff_head * tmp_t;
+		}
+		if (idx_x + 1 < COLS) {
+		    diff_head = d_ca.head[idx_g + 1] - d_ca.head[idx_g];
+		    tmp_t = d_ca.K[idx_g] * THICKNESS;
+		    Q += diff_head * tmp_t;
+		}
+		if (idx_y + 1 < ROWS) {
+		    diff_head = d_ca.head[(idx_y + 1) * COLS + idx_x] - d_ca.head[idx_g];
+		    tmp_t = d_ca.K[idx_g] * THICKNESS;
+		    Q += diff_head * tmp_t;
+		}
+	    }
+
+	    Q -= d_ca.Source[idx_g];
+
+	    double ht1 = Q * DELTA_T;
+	    double ht2 = AREA * d_ca.Sy[idx_g];
+
+	    d_write_head[idx_g] = d_ca.head[idx_g] + ht1 / ht2;
 	}
+    }
 }
 
 __global__ void find_active_cells_kernel(struct CA d_ca) {
-	unsigned idx_x = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned idx_y = blockIdx.y * blockDim.y + threadIdx.y;
-	unsigned idx_g = idx_y * COLS + idx_x;
+    unsigned idx_x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned idx_y = blockIdx.y * blockDim.y + threadIdx.y;
+    unsigned idx_g = idx_y * COLS + idx_x;
 
-	if (idx_x < ROWS && idx_y < COLS) {
-		if (d_ca.head[idx_g] < headFixed || d_ca.Source[idx_g] != 0) {
-			my_push_back(idx_g);
-			return;
-		}
-		if (idx_x >= 1) {
-			if (d_ca.head[idx_g - 1] < headFixed) {
-				my_push_back(idx_g);
-				return;
-			}
-		}
-		if (idx_y >= 1) {
-			if (d_ca.head[idx_g - COLS] < headFixed) {
-				my_push_back(idx_g);
-				return;
-			}
-		}
-		if (idx_x + 1 < COLS) {
-			if (d_ca.head[idx_g + 1] < headFixed) {
-				my_push_back(idx_g);
-				return;
-			}
-		}
-		if (idx_y + 1 < ROWS) {
-			if (d_ca.head[idx_g + COLS] < headFixed) {
-				my_push_back(idx_g);
-				return;
-			}
-		}
+    if (idx_x < ROWS && idx_y < COLS) {
+	if (d_ca.head[idx_g] < headFixed || d_ca.Source[idx_g] != 0) {
+	    my_push_back(idx_g);
+	    return;
 	}
+	if (idx_x >= 1) {
+	    if (d_ca.head[idx_g - 1] < headFixed) {
+		my_push_back(idx_g);
+		return;
+	    }
+	}
+	if (idx_y >= 1) {
+	    if (d_ca.head[idx_g - COLS] < headFixed) {
+		my_push_back(idx_g);
+		return;
+	    }
+	}
+	if (idx_x + 1 < COLS) {
+	    if (d_ca.head[idx_g + 1] < headFixed) {
+		my_push_back(idx_g);
+		return;
+	    }
+	}
+	if (idx_y + 1 < ROWS) {
+	    if (d_ca.head[idx_g + COLS] < headFixed) {
+		my_push_back(idx_g);
+		return;
+	    }
+	}
+    }
 }
 
 void perform_simulation_on_GPU() {
-	dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
 
-	const int blockCount = ceil((ROWS * COLS) / (BLOCK_SIZE * BLOCK_SIZE));
-	int gridSize = ceil(sqrt(blockCount));
-	dim3 gridDim(gridSize, gridSize);
+    const int blockCount = ceil((ROWS * COLS) / (BLOCK_SIZE * BLOCK_SIZE));
+    int gridSize = ceil(sqrt(blockCount));
+    dim3 gridDim(gridSize, gridSize);
 
-	int activeBlockCount, activeGridSize;
+    int activeBlockCount, activeGridSize;
 
-	Timer stepTimer;
+    Timer stepTimer;
 
     startTimer(&stepTimer);
-	bool isWholeGridActive = false;
+    bool isWholeGridActive = false;
 
-	for (int i = 0; i < SIMULATION_ITERATIONS; i++) {
+    for (int i = 0; i < SIMULATION_ITERATIONS; i++) {
 
-		dim3 *simulationGridDim;
-		if (!isWholeGridActive) {
+	dim3 *simulationGridDim;
+	if (!isWholeGridActive) {
 
-			dev_active_cells_count = 0;
-			find_active_cells_kernel <<<gridDim, blockSize>>>(d_read);
-			cudaDeviceSynchronize();
+	    dev_active_cells_count = 0;
+	    find_active_cells_kernel <<<gridDim, blockSize>>>(d_read);
+	    cudaDeviceSynchronize();
 
-			isWholeGridActive = dev_active_cells_count == ROWS*COLS;
+	    isWholeGridActive = dev_active_cells_count == ROWS*COLS;
 
-			activeBlockCount = ceil((double) dev_active_cells_count / (BLOCK_SIZE * BLOCK_SIZE));
-			activeGridSize = ceil(sqrt(activeBlockCount));
-			dim3 activeGridDim(activeGridSize, activeGridSize);
+	    activeBlockCount = ceil((double) dev_active_cells_count / (BLOCK_SIZE * BLOCK_SIZE));
+	    activeGridSize = ceil(sqrt(activeBlockCount));
+	    dim3 activeGridDim(activeGridSize, activeGridSize);
 
-			simulationGridDim = &activeGridDim;
-		} else {
-			simulationGridDim = &gridDim;
-		}
-
-		simulation_step_kernel <<<*simulationGridDim, blockSize>>>(d_read, d_write.head);
-		cudaDeviceSynchronize();
-
-		double *tmp = d_write.head;
-		d_write.head = d_read.head;
-		d_read.head = tmp;
-
-        if (i % STATISTICS_WRITE_FREQ == 0) {
-            endTimer(&stepTimer);
-            stats[i].coverage = double(dev_active_cells_count * 100) / (ROWS * COLS);
-            stats[i].stepTime = getElapsedTime(startTimer);
-            startTimer(&stepTimer);
-        }
+	    simulationGridDim = &activeGridDim;
+	} else {
+	    simulationGridDim = &gridDim;
 	}
+
+	simulation_step_kernel <<<*simulationGridDim, blockSize>>>(d_read, d_write.head);
+	cudaDeviceSynchronize();
+
+	double *tmp = d_write.head;
+	d_write.head = d_read.head;
+	d_read.head = tmp;
+
+	if (i % STATISTICS_WRITE_FREQ == 0) {
+	    endTimer(&stepTimer);
+	    stats[i].coverage = double(dev_active_cells_count * 100) / (ROWS * COLS);
+	    stats[i].stepTime = getElapsedTime(stepTimer);
+	    startTimer(&stepTimer);
+	}
+    }
 }
 
 int main(int argc, char *argv[]) {
-	allocate_memory();
-	init_read_ca();
-	init_write_head();
+    allocate_memory();
+    init_read_ca();
+    init_write_head();
 
-	perform_simulation_on_GPU();
+    perform_simulation_on_GPU();
 
-	if (WRITE_OUTPUT_TO_FILE) {
-		write_heads_to_file(d_write.head, argv[0]);
-	}
+    if (WRITE_OUTPUT_TO_FILE) {
+	write_heads_to_file(d_write.head, argv[0]);
+    }
 
-	if (WRITE_STATISTICS_TO_FILE) {
-		write_statistics_to_file(stats, argv[0]);
-	}
+    if (WRITE_STATISTICS_TO_FILE) {
+	write_statistics_to_file(stats, argv[0]);
+    }
 
-	return 0;
+    return 0;
 }
 
 
--- src/parallel/unified_memory/unified_memory_loop.cu
+++ src/parallel/unified_memory/unified_memory_loop.cu
@@ -10,39 +10,39 @@ __global__ void simulation_step_kernel(struct CA d_ca, double *d_write_head) {
     double Q, diff_head, tmp_t, ht1, ht2;
 
     if (idx_x < COLS && idx_y < ROWS) {
-        if (idx_y != 0 && idx_y != ROWS - 1) {
-            for (int i = 0; i < KERNEL_LOOP_SIZE; i++) {
-	            if (i == KERNEL_LOOP_SIZE - 1){
-		            Q = 0;
-	            }
-                if (idx_x >= 1) {
-                    diff_head = d_ca.head[idx_g - 1] - d_ca.head[idx_g];
-                    tmp_t = d_ca.K[idx_g] * THICKNESS;
-                    Q += diff_head * tmp_t;
-                }
-                if (idx_y >= 1) {
-                    diff_head = d_ca.head[(idx_y - 1) * COLS + idx_x] - d_ca.head[idx_g];
-                    tmp_t = d_ca.K[idx_g] * THICKNESS;
-                    Q += diff_head * tmp_t;
-                }
-                if (idx_x + 1 < COLS) {
-                    diff_head = d_ca.head[idx_g + 1] - d_ca.head[idx_g];
-                    tmp_t = d_ca.K[idx_g] * THICKNESS;
-                    Q += diff_head * tmp_t;
-                }
-                if (idx_y + 1 < ROWS) {
-                    diff_head = d_ca.head[(idx_y + 1) * COLS + idx_x] - d_ca.head[idx_g];
-                    tmp_t = d_ca.K[idx_g] * THICKNESS;
-                    Q += diff_head * tmp_t;
-                }
-            }
+	if (idx_y != 0 && idx_y != ROWS - 1) {
+	    for (int i = 0; i < KERNEL_LOOP_SIZE; i++) {
+		if (i == KERNEL_LOOP_SIZE - 1){
+		    Q = 0;
+		}
+		if (idx_x >= 1) {
+		    diff_head = d_ca.head[idx_g - 1] - d_ca.head[idx_g];
+		    tmp_t = d_ca.K[idx_g] * THICKNESS;
+		    Q += diff_head * tmp_t;
+		}
+		if (idx_y >= 1) {
+		    diff_head = d_ca.head[(idx_y - 1) * COLS + idx_x] - d_ca.head[idx_g];
+		    tmp_t = d_ca.K[idx_g] * THICKNESS;
+		    Q += diff_head * tmp_t;
+		}
+		if (idx_x + 1 < COLS) {
+		    diff_head = d_ca.head[idx_g + 1] - d_ca.head[idx_g];
+		    tmp_t = d_ca.K[idx_g] * THICKNESS;
+		    Q += diff_head * tmp_t;
+		}
+		if (idx_y + 1 < ROWS) {
+		    diff_head = d_ca.head[(idx_y + 1) * COLS + idx_x] - d_ca.head[idx_g];
+		    tmp_t = d_ca.K[idx_g] * THICKNESS;
+		    Q += diff_head * tmp_t;
+		}
+	    }
 
-            Q -= d_ca.Source[idx_g];
-            ht1 = Q * DELTA_T;
-            ht2 = AREA * d_ca.Sy[idx_g];
+	    Q -= d_ca.Source[idx_g];
+	    ht1 = Q * DELTA_T;
+	    ht2 = AREA * d_ca.Sy[idx_g];
 
-            d_write_head[idx_g] = d_ca.head[idx_g] + ht1 / ht2;
-        }
+	    d_write_head[idx_g] = d_ca.head[idx_g] + ht1 / ht2;
+	}
     }
 }
 
@@ -52,24 +52,23 @@ void perform_simulation_on_GPU() {
     double gridSize = ceil(sqrt(blockCount));
     dim3 blockCount2D(gridSize, gridSize);
 
-	Timer stepTimer;
+    Timer stepTimer;
     startTimer(&stepTimer);
 
-	for (int i = 0; i < SIMULATION_ITERATIONS; i++) {
+    for (int i = 0; i < SIMULATION_ITERATIONS; i++) {
 
-	    simulation_step_kernel << < blockCount2D, blockSize >> > (d_read, d_write.head);
-        cudaDeviceSynchronize();
+	simulation_step_kernel << < blockCount2D, blockSize >> > (d_read, d_write.head);
+	cudaDeviceSynchronize();
 
-        double *tmp1 = d_write.head;
-        d_write.head = d_read.head;
-        d_read.head = tmp1;
+	double *tmp1 = d_write.head;
+	d_write.head = d_read.head;
+	d_read.head = tmp1;
 
-        if (i % STATISTICS_WRITE_FREQ == 0) {
-            endTimer(&stepTimer);
-            stats[i].coverage = double(dev_active_cells_count * 100) / (ROWS * COLS);
-            stats[i].stepTime = getElapsedTime(startTimer);
-            startTimer(&stepTimer);
-        }
+	if (i % STATISTICS_WRITE_FREQ == 0) {
+	    endTimer(&stepTimer);
+	    stats[i].stepTime = getElapsedTime(stepTimer);
+	    startTimer(&stepTimer);
+	}
     }
 }
 
@@ -80,13 +79,13 @@ int main(int argc, char *argv[]) {
 
     perform_simulation_on_GPU();
 
-	if(WRITE_OUTPUT_TO_FILE){
-		write_heads_to_file(d_write.head, argv[0]);
-	}
+    if(WRITE_OUTPUT_TO_FILE){
+	write_heads_to_file(d_write.head, argv[0]);
+    }
 
-	if (WRITE_STATISTICS_TO_FILE) {
-		write_statistics_to_file(stats, argv[0]);
-	}
+    if (WRITE_STATISTICS_TO_FILE) {
+	write_statistics_to_file(stats, argv[0]);
+    }
 
     return 0;
 }
