286732f6ce3af2a7688cddc80f52630b00006e54
Tomasz Pecak <pecatoma@gmail.com>
2020-07-27 00:46:32 +0200

    Remove coverage multiplying by 100 in unified_active_cells_loop.cu

--- src/parallel/active_cells/unified/unified_active_cells_loop.cu
+++ src/parallel/active_cells/unified/unified_active_cells_loop.cu
@@ -140,7 +140,7 @@ void perform_simulation_on_GPU() {
 
         if (i % STATISTICS_WRITE_FREQ == 0) {
             endTimer(&stepTimer);
-            stats[i].coverage = double(dev_active_cells_count * 100) / (ROWS * COLS);
+	        stats[i].coverage = double(dev_active_cells_count) / (ROWS * COLS);
             stats[i].stepTime = getElapsedTime(stepTimer);
             startTimer(&stepTimer);
         }
